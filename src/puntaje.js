73c1a6f140a7f155f428941c36fdf23cb52325ec
nathan95oks <nathanaelalba75@gmail.com>
2025-03-06 09:04:34 -0400

    si uno de los jugadores hace un punto obtiene 15

--- src/puntaje.js
+++ src/puntaje.js
@@ -1,6 +1,12 @@
 function puntaje (j1,j2){
- if(j1 ===0&&j2 ===0){
+ if(j1 ===0&&j2 ===0)
     return ['love', 'love'];
- };
+ 
+if((j1 === 0 && j2 === 1))
+    return [0, 15];
+
+if((j1 === 1 && j2 === 0 ))
+    return [15,0];
 }
+
 export default puntaje;
--- src/puntaje.test.js
+++ src/puntaje.test.js
@@ -3,5 +3,14 @@ import puntaje from './puntaje.js';
 describe('debería devolver el puntaje de la partida', () => {
   it('debería devolver love si el puntaje devuelto es 0', () => {
     expect(puntaje(0, 0)).toEqual(['love', 'love']);
-  })
+  });
+
+  it('debería devolver love si el puntaje devuelto es 0 y 15 si se logro hacer un punto ', () => {
+    expect(puntaje(0, 1)).toEqual([0, 15]);
+  });
+
+  it('debería devolver love si el puntaje devuelto es 0 y 15 si se logro hacer un punto ', () => {
+    expect(puntaje(1 , 0 )).toEqual([15, 0]);
+  });
+
 });
